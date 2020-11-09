239ab44a8e9b93d5769105dd77b31b2077cb2d81
jhagspiel <j.hagspiel@student.tudelft.nl>
2020-11-09 18:20:38 +0100

    Initial commit

---
+++ src/digital_delft_blue/application.py
@@ -0,0 +1,7 @@
+
+class Application:
+
+    #def _init_(self):
+
+    def start(self):
+        print("Here we go again")
---
+++ src/digital_delft_blue/main.py
@@ -0,0 +1,18 @@
+"""
+Entry module of the application.
+"""
+
+from digital_delft_blue.application import Application
+
+
+def main() -> None:
+	"""
+	Entry function of the application.
+
+	:return: Nothing.
+	"""
+	Application().start()
+
+
+if __name__ == '__main__':
+	main()
